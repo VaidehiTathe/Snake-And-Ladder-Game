d2c78579758af68df744d52ca61fd27a5f15151b
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-12 21:37:40 +0530

    [Vaidehi]ADD:checking condition for player

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,5 +1,32 @@
+
 echo "Welcome To Snake And Ladder Game"
 startPosition=0
+currentPosition=0
+lastPosition=100
+ladder=1
+snake=2
+noplay=3
 
-rollDie=$((RANDOM%6+1))
-echo "Die number is:" $rollDie
+function play()
+{
+	rollDie=$((RANDOM%6+1))
+	echo "Die number is:" $rollDie
+
+	option=$((RANDOM%3+1))
+
+	if [[ $option -eq $ladder ]]
+	then
+		currentPosition=`expr $(($currentPosition + $rollDie ))`
+	elif [[ $option -eq $snake ]]
+	then
+		currentPosition=$(( $currentPosition-$rollDie ))
+	else
+		currentPosition=$currentPosition
+		exit
+	fi
+echo "current position is:"$currentPosition
+
+}
+
+play
+
