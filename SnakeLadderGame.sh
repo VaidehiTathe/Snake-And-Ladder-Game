e2d1be965c8a13fb1961692970ac7bcf9091f720
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-13 23:39:41 +0530

    [Vaidehi]ADD:condition to get exact winning position

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash +x
 echo "Welcome To Snake And Ladder Game"
 startPosition=0
 currentPosition=0
@@ -6,33 +6,60 @@ lastPosition=100
 ladder=1
 snake=2
 noplay=3
+required=0
 
-function play()
+function ladder()
 {
-	rollDie=$((RANDOM%6+1))
-	echo "Die number is:" $rollDie
 
-	option=$((RANDOM%3+1))
-
-	if [[ $option -eq $ladder ]]
+	required=$(($lastPosition-$currentPosition))
+	echo "required number is:"$required
+	if [[ $required -ge $rollDie ]] 
 	then
-		currentPosition=`expr $(($currentPosition + $rollDie ))`
-	elif [[ $option -eq $snake ]]
-	then
-		currentPosition=$(( $currentPosition-$rollDie ))
-	elif [[ $currentPosition -lt 0 ]]
-	then
-		currentPosition=$startPosition
-	else
-		currentPosition=$currentPosition
-		#exit
+		  currentPosition=`expr $(($currentPosition + $rollDie))`
 	fi
-echo "current position is:"$currentPosition
+	echo "current position for ladder is:"$currentPosition
 
 }
+function snake()
+{
+	if [[ $currentPosition -lt 0 ]]
+        then
+               currentPosition=$startPosition
+	else
+        	currentPosition=`expr $(($currentPosition - $rollDie))`
+		echo "current position for snake is:"$currentPosition
+	fi
+}
+function noplay()
+{
+	currentPosition=$currentPosition
+        echo "current position for noplay is:"$currentPosition
+}
+function play()
+{
+	option=$((RANDOM%3+1))
+	case $option in
+		$ladder)
+			ladder
+			;;
+		$snake)
+			snake
+			;;
+		$noplay)
+			noplay
+	esac
+}
 
-while [[ $currentPosition -le $lastPosition ]]
-do
-        play
-done
 
+
+function start()
+{
+	while [[ $currentPosition -lt $lastPosition ]]
+	do
+		rollDie=$((RANDOM%6+1))
+        	echo "Die number is:" $rollDie
+       		play
+	done
+}
+start
+echo "You won the game"
